91a5a18a423adc39076d9a9cba345b3cbb4e856e
Benli <e@example.com>
2024-12-01 23:01:05 +0200

    add return to functions

--- app/src/main/java/com/benliandamit/tictactoe/util/GameUtilities.kt
+++ app/src/main/java/com/benliandamit/tictactoe/util/GameUtilities.kt
@@ -1,9 +1,6 @@
-import java.util.*
-import kotlin.collections.ArrayList
+package main.util
 
 object GameUtils {
-    const val PLAYER_X = "X"
-    const val PLAYER_O = "O"
 
     fun makeMove(board: Array<CharArray>, player: Char, row: Int, col: Int): Boolean {
         if (row in 0..2 && col in 0..2 && board[row][col] == ' ') {
@@ -13,26 +10,21 @@ object GameUtils {
         return false
     }
 
-    fun checkGameState(board: Array<CharArray>): String {
+    fun checkGameState(board: Array<CharArray>): StateEnum {
         for (i in 0..2) {
             if (board[i][0] != ' ' && board[i][0] == board[i][1] && board[i][1] == board[i][2])
-                return "${board[i][0]} wins"
+                return StateEnum.fromValue(board[i][0]);
             else if (board[0][i] != ' ' && board[0][i] == board[1][i] && board[1][i] == board[2][i])
-                return "${board[0][i]} wins"
+                return StateEnum.fromValue(board[0][1]);
         }
 
         if (board[0][0] != ' ' && board[0][0] == board[1][1] && board[1][1] == board[2][2])
-            return "${board[0][0]} wins"
+            return StateEnum.fromValue(board[0][0]);
         else if (board[0][2] != ' ' && board[0][2] == board[1][1] && board[1][1] == board[2][0])
-            return "${board[0][2]} wins"
+            return StateEnum.fromValue(board[0][2]);
         else if (board.all { row -> row.all { cell -> cell != ' ' } })
-            return "It's a tie"
+            return StateEnum.TIE;
 
-        return "Place next move"
+        return StateEnum.CONTINUE;
     }
-
-    fun printBoard(board: Array<CharArray>) {
-        for (row in board)
-            println(row.joinToString(" | "))
-        println()
-    }
+}
